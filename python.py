29cafbfba1e724a3d1f4414a963ad85317c27e6b
zufarbekjumayev <jumayevzufarbek829@gmail.com>
2024-12-05 16:23:34 +0300

    python3

---
+++ python.py
@@ -0,0 +1,38 @@
+# yosh = 16
+# print(yosh)
+# ism = "Zufarbek"
+# meva = "olma"
+# print(ism)
+# print(meva)
+# menhaqimda = "Salom! Men haqimda ma'lumot:"
+# print(menhaqimda)
+# ism = "ismim: Joe"
+# sana = "Tug'ilgan kunim: 2008-yil 26-noyabr"
+# joy = "Tug'ilgan joyim: Buxoro viloyati"
+# kasbim = "Kasbim: O'quvchi"
+# print(ism)
+# print(sana)
+# print(joy)
+# print(kasbim)
+# a = "Men haqimda qisqacha ma'lumot:"
+# print(a)
+# ism = "Zufarbek"
+# print("Ismim:", ism)
+# yosh = 16
+# print("Yoshim:", yosh, "yosh")
+# boy = 1.86
+# print("Bo'yim:", boy, "metr")
+# b = True
+# print("Dasturlashni yoqtiramanmi?", b)
+a = "Joe"
+print("Buyurtmachi:", a)
+b = "Burger"
+print("Buyurtma nomi:", b)
+miqdor = 2
+print("Miqdori:", miqdor)
+narx = 5.99
+print("Bitta taom narxi:", narx, "USD")
+umumiy = 2 * 5.99 
+print("Umumiy narx:", umumiy, "USD")
+c = True
+print("Yetkazib berish kereakmi?", c)
